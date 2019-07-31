07e9ea7ec2a6a393c6c9b24aa4fc1a5e2aaeb6e3
jivKrishna <krishnapandit1997@outlook.com>
2019-07-31 23:33:47 +0530

    CRUD created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+  end
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html
+
+      format.pdf { render pdf: "users", template: "users/users_data.html.erb", layout: "pdf.html.erb"}
+
+      format.csv { send_data @users.to_csv, filename: "users-#{Time.now.strftime("%H%M%S")}.csv"}
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :mobile_no)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
 end
