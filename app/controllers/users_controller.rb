0e0e72b29b03d4ceeb696495b55c0e2544ebca8d
jivKrishna <krishnapandit1997@outlook.com>
2019-08-01 01:53:31 +0530

    CRUD operation completed!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+    if @user.save
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
